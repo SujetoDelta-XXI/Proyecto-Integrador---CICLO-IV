ac34ef7cc42d373e3d85296f8dc2e316b174e1a6
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-13 15:46:02 -0500

    Correción - rutas

--- user-frontend/src/components/interfaces/Header.jsx
+++ user-frontend/src/components/interfaces/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
       <div className="main-header-logo">EstiloYa</div>
       <nav className="main-header-nav">
         <Link to="/tienda" className="main-header-link">Tienda</Link>
-        <Link to="/loginForm" className="main-header-link">Login</Link>
+        <Link to="/login" className="main-header-link">Login</Link>
         <Link to="/register" className="main-header-link">Registro</Link>
         {/* Agrega más enlaces si lo necesitas */}
       </nav>
