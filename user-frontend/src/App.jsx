a38642cafd9ae2c24b7b9811030493a6db911731
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-25 16:38:47 -0500

    Interface - Eliminado

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -2,11 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Login from "./components/interfaces/Login";
-import Footer from "./components/interfaces/Footer";
-import Register from "./components/interfaces/Register";
-import LoginForm from "./components/interfaces/LoginForm";
-import Index from "./components/interfaces/Index";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import LoginForm from "./components/LoginForm";
+import Index from "./components/Index";
 
 function App() {
   return (
