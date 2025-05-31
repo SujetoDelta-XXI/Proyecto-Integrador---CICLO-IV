055f64ee48a783e690aae30f24ff58f1d5d38911
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-30 23:46:27 -0500

    a

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -11,8 +11,6 @@ import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 
 import ShopGridPage from "./components/Producto/ShopGridPage";
-import ForgotPassword from "./components/ForgotPassword";
-import ResetPassword from "./components/ResetPassword";
 
 function App() {
   return (
@@ -28,10 +26,9 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/productos" element={<ShopGridPage />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
