f7850b16877b9d266d733aad1aa1622eb568ddf8
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-07-07 07:37:31 -0500

    Despliegue

--- usuario-api/target/generated-sources/annotations/com/example/usuario_api/mapper/PedidoMapperImpl.java
+++ usuario-api/target/generated-sources/annotations/com/example/usuario_api/mapper/PedidoMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-07T03:01:46-0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
+    date = "2025-07-07T07:34:20-0500",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.50.v20250628-1110, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
 public class PedidoMapperImpl implements PedidoMapper {
