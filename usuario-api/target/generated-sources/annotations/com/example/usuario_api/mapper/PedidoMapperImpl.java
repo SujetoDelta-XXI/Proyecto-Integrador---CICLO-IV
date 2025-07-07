f2853882884a7de0aedce4962b7bea3bf377e94d
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-07-07 02:46:10 -0500

    Correcion despliegue

---
+++ usuario-api/target/generated-sources/annotations/com/example/usuario_api/mapper/PedidoMapperImpl.java
@@ -0,0 +1,33 @@
+package com.example.usuario_api.mapper;
+
+import com.example.usuario_api.dto.PedidoDto;
+import com.example.usuario_api.model.Pedido;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-07T02:40:56-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
+)
+@Component
+public class PedidoMapperImpl implements PedidoMapper {
+
+    @Override
+    public PedidoDto toDto(Pedido p) {
+        if ( p == null ) {
+            return null;
+        }
+
+        PedidoDto pedidoDto = new PedidoDto();
+
+        pedidoDto.setId( p.getId() );
+        pedidoDto.setFecha_pedido( p.getFecha_pedido() );
+        pedidoDto.setTotal( p.getTotal() );
+        pedidoDto.setEstado( p.getEstado() );
+        pedidoDto.setDireccion_envio( p.getDireccion_envio() );
+        pedidoDto.setTelefono( p.getTelefono() );
+
+        return pedidoDto;
+    }
+}
---
+++ usuario-api/target/generated-sources/annotations/com/example/usuario_api/mapper/UsuarioMapperImpl.java
@@ -0,0 +1,51 @@
+package com.example.usuario_api.mapper;
+
+import com.example.usuario_api.dto.UsuarioDto;
+import com.example.usuario_api.model.Usuario;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-07T02:40:55-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
+)
+@Component
+public class UsuarioMapperImpl implements UsuarioMapper {
+
+    @Override
+    public UsuarioDto toDto(Usuario usuario) {
+        if ( usuario == null ) {
+            return null;
+        }
+
+        UsuarioDto usuarioDto = new UsuarioDto();
+
+        usuarioDto.setId( usuario.getId() );
+        usuarioDto.setNombre( usuario.getNombre() );
+        usuarioDto.setApellidos( usuario.getApellidos() );
+        usuarioDto.setCorreo( usuario.getCorreo() );
+        usuarioDto.setTelefono( usuario.getTelefono() );
+        usuarioDto.setRol( usuario.getRol() );
+
+        return usuarioDto;
+    }
+
+    @Override
+    public Usuario toEntity(UsuarioDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Usuario usuario = new Usuario();
+
+        usuario.setId( dto.getId() );
+        usuario.setNombre( dto.getNombre() );
+        usuario.setApellidos( dto.getApellidos() );
+        usuario.setCorreo( dto.getCorreo() );
+        usuario.setTelefono( dto.getTelefono() );
+        usuario.setRol( dto.getRol() );
+
+        return usuario;
+    }
+}
